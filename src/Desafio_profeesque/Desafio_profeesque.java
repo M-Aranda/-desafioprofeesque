6eab42132be0271d8054d4d60bbdb7e8d066ad37
M-Aranda <cheloz_20@hotmail.com>
2017-11-12 10:51:08 -0300

    Creadas listas para valores de x e y en cada coordenada, implementada enumeracion en el pedido de datos. Pruebas chicas de uso de listas al final. Comentarios son para guia propia, los voy a borrar antes de la entrega final

--- src/Desafio_profeesque/Desafio_profeesque.java
+++ src/Desafio_profeesque/Desafio_profeesque.java
@@ -1,56 +1,76 @@
-
 package Desafio_profeesque;
 
 import Mensaje_inicial.Mensaje_de_inicio;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Desafio_profeesque {
 
-
     public static void main(String[] args) {
-        
+
         Scanner respuesta = new Scanner(System.in);
-        
+
         int eleccion;
-        while(true){
+        while (true) {
             try {
                 System.out.println("3 o 4 puntos?");
-                eleccion=Integer.parseInt(respuesta.nextLine());
-                
-                if (eleccion==3 || eleccion==4){
-                break;
-            }
-                
+                eleccion = Integer.parseInt(respuesta.nextLine());
+
+                if (eleccion == 3 || eleccion == 4) {
+                    break;
+                }
+
             } catch (Exception e) {
                 System.out.println("Debe responder 3 o 4");
             }
-}
-        Mensaje_de_inicio mensaje=new Mensaje_de_inicio();
+        }
+        Mensaje_de_inicio mensaje = new Mensaje_de_inicio();
         mensaje.mostrarMensajeDeInicio();
 
-        Scanner input= new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+
+        List<Integer> valoresX = new ArrayList<>();//lista para valor x de coordenada
+        List<Integer> valoresY = new ArrayList<>();//lista para valor y de coordenada
+
         int x;
         int y;
-        
-        int contador=0;
-        while(true){
+
+        int contador = 0;
+        while (true) {
             try {
-                String a=input.nextLine();
-                    String[]valores=a.split(",");
-                    String xs=valores[0];
-                    String ys=valores[1];
-                    x=Integer.parseInt(xs);
-                    y=Integer.parseInt(ys);
-                    contador++;
-                    
+                System.out.print("Ingrese coordenada Nº" + (contador + 1) + ":");
+                String a = input.nextLine();
+                String[] valores = a.split(",");
+                String xs = valores[0];
+                String ys = valores[1];
+                x = Integer.parseInt(xs);
+                y = Integer.parseInt(ys);
+
+                valoresX.add(x);
+                valoresY.add(y);
+                contador++;
+
             } catch (Exception e) {
                 System.out.println("Debe ingresar una coordenada valida");
             }
-            if(eleccion==contador){
+            if (eleccion == contador) {
                 break;
             }
         }
+        
+        System.out.println(valoresX.get(0));//sout de prueba
+        //recordar que listadenombre.get(numero de indice); es para mostrar el valor de ese indice
+
+        for (int i = 0; i < valoresX.size(); i++) {//for de prueba
+            System.out.println("Valor de X de coordenada Nº" + (i + 1) + ":" + valoresX.get(i));
+        }
+
+        for (int i = 0; i < valoresY.size(); i++) {//for de prueba
+            System.out.println("Valor de Y de coordeada Nº" + (i + 1) + ":" + valoresY.get(i));
+        }
+
     }
-    
+
 }
