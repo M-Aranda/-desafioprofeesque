702a41bdde974fe1b63ba9f1ba1f88f6ea96957c
M-Aranda <cheloz_20@hotmail.com>
2017-11-15 21:07:34 -0300

    Creada clase pendiente para determinar que las coordenadas ingresadas no formen solo una linea, pero no se usa todavia. Agregado uso de recolector de basura, contador de figuras mejorado, ahora envia mensaje que varia dependiendo de si es una figura o mas. Corregido retorno nulo de valores perimetro, area y getters debido a la no inicializacion de los lados en las clases superiores. Implementada esta correccion en todas las clases que son extensiones de triangulos o cuadrilateros. Posibles mejora en la herencia e interfase divisadas; note mas cosas en comun entre las clases que hay

--- src/Figuras/Cuadrilatero.java
+++ src/Figuras/Cuadrilatero.java
@@ -6,5 +6,17 @@ public class Cuadrilatero {
     public double lado2;
     public double lado3;
     public double lado4;
+    
+    
+    public Cuadrilatero(){}
+
+    public Cuadrilatero(double lado1, double lado2, double lado3, double lado4) {
+        this.lado1 = lado1;//inicializa lado1
+        this.lado2 = lado2;//inicializa lado2
+        this.lado3 = lado3;//inicializa lado3
+        this.lado4 = lado4;//inicializa lado4
+    }
+    
 
 }
+
--- src/Paralelogramos/Rombo.java
+++ src/Paralelogramos/Rombo.java
@@ -1,20 +1,36 @@
-
 package Paralelogramos;
 
 import Figuras.Cuadrilatero;
 import Figuras.Figureable;
 
-
-public class Rombo extends Cuadrilatero implements Figureable{
+public class Rombo extends Cuadrilatero implements Figureable {
 
     public Rombo() {
     }
 
-    public Rombo(double lado1,double lado2, double lado3,double lado4){}
+    public Rombo(double lado1, double lado2, double lado3, double lado4) {
+        super(lado1, lado2, lado3, lado4);
+    }
+
+    public double getLado1() {
+        return lado1;
+    }
+
+    public double getLado2() {
+        return lado2;
+    }
+
+    public double getLado3() {
+        return lado3;
+    }
+
+    public double getLado4() {
+        return lado4;
+    }
 
     @Override
     public double calcularPerimetro() {
-        double perimetro= (lado1*4);
+        double perimetro = (getLado1() * 4);
         return perimetro;
     }
 
@@ -22,5 +38,18 @@ public class Rombo extends Cuadrilatero implements Figureable{
     public double calcularArea() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    public void mostrarInformacion() {
+        System.out.println("Las coordenadas ingresadas forman un cuadrilatero rombo");
+        System.out.println("Perimetro: " + calcularPerimetro());
+        System.out.println("Area: " + calcularArea());
+
+        System.out.println("Valores de lados: ");
+        System.out.println("Lado 1: " + getLado1());
+        System.out.println("Lado 2: " + getLado2());
+        System.out.println("Lado 3: " + getLado3());
+        System.out.println("Lado 4: "+ getLado4());
+
+    }
+
 }
